0bd675e564a0417c7e5b01d45e7432b6b2119ced
walker213 <921254299@qq.com>
2020-09-05 02:03:52 +0800

    feat(button): add Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type="primary" size="lg">
+        primary
+      </Button>
+      <Button type="danger">danger</Button>
+      <Button type="link" href="https://www.baidu.com">
+        百度
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import classnames from 'classnames';
+
+function addClassPrefixHOF(prefix: string) {
+  return (name?: string): string => {
+    return [prefix, name].filter(Boolean).join('-');
+  };
+}
+
+const acp = addClassPrefixHOF('bui-btn');
+
+type ButtonSize = 'lg' | 'sm';
+type ButtonType = 'primary' | 'default' | 'danger' | 'link';
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  type?: ButtonType;
+  href?: string;
+}
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+  const { type, disabled, size, href, className, children, ...restProps } = props;
+  const btnClassName = classnames(acp(), acp(size), acp(type), className);
+  if (type === 'link' && href) {
+    return (
+      <a href={href} className={btnClassName}>
+        {children}
+      </a>
+    );
+  }
+  return (
+    <button type="button" className={btnClassName} disabled={disabled} {...restProps}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  disabled: false,
+  type: 'default',
+};
+
+export default Button;
