114775ea1065e9a829eaa8ae93905e0f9cc869fd
walker213 <921254299@qq.com>
2020-09-07 21:28:56 +0800

    feat(menu): add Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import Button from './components/Button';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div>
-      <Button btnType="primary">primary</Button>
-      <Button btnType="danger">danger</Button>
-      <Button btnType="default" size="lg">
-        default && lg
-      </Button>
-      <Button disabled size="sm">
-        disabled && sm
-      </Button>
-      <Button btnType="link" href="https://www.baidu.com" target="_blank">
-        百度
-      </Button>
-      <Button btnType="link">百度無連接</Button>
-      <Button btnType="link" href="https://www.baidu.com" disabled>
-        disabled link
-      </Button>
+      <section>
+        <h1>Button</h1>
+        <Button btnType="primary">primary</Button>
+        <Button btnType="danger">danger</Button>
+        <Button btnType="default" size="lg">
+          default && lg
+        </Button>
+        <Button disabled size="sm">
+          disabled && sm
+        </Button>
+        <Button btnType="link" href="https://www.baidu.com" target="_blank">
+          百度
+        </Button>
+        <Button btnType="link">百度無連接</Button>
+        <Button btnType="link" href="https://www.baidu.com" disabled>
+          disabled link
+        </Button>
+      </section>
+      <section>
+        <h1>Menu</h1>
+        <Menu />
+      </section>
     </div>
   );
 }
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import classnames from 'classnames';
+import { addClassPrefixHOF } from '../../utils';
+
+const getFullClassName = addClassPrefixHOF('bui-menu');
+
+type MenuMode = 'vertical' | 'horizontal';
+export interface MenuProps {
+  defaultIndex?: number;
+  mode?: MenuMode;
+  style?: React.CSSProperties;
+  onSelect?: (selectedIndex: number) => void;
+  className?: string;
+}
+
+const Menu: React.FC<MenuProps> = (props) => {
+  const { defaultIndex, mode, style, onSelect, className, children, ...restProps } = props;
+  const classes = classnames(
+    getFullClassName(),
+    { [getFullClassName('verticla')]: mode === 'vertical' },
+    className,
+  );
+  return (
+    <ul className={classes} style={style} {...restProps}>
+      <li>menu item1</li>
+      <li>menu item2</li>
+      <li>menu item3</li>
+    </ul>
+  );
+};
+
+Menu.defaultProps = {
+  defaultIndex: 0,
+  mode: 'horizontal',
+};
+
+export default Menu;
