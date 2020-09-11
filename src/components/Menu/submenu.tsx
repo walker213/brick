d0fa0c1d21d4e9d51fadb4aa9dc8c48da415daa0
walker213 <921254299@qq.com>
2020-09-11 21:20:23 +0800

    feat(menu): add rote animation on SubMenu's icon

--- src/components/Menu/submenu.tsx
+++ src/components/Menu/submenu.tsx
@@ -2,6 +2,7 @@ import React, { useContext, FunctionComponentElement, useState } from 'react';
 import classnames from 'classnames';
 import { MenuContext } from './menu';
 import { MenuItemProps } from './menuItem';
+import Icon from '../Icon/icon';
 import { addClassPrefixHOF } from '../../utils';
 
 const getFullClassName = addClassPrefixHOF('bui-menu-submenu');
@@ -20,7 +21,7 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
   const [menuOpen, setmenuOpen] = useState(isOpen);
   const classes = classnames(
     getFullClassName(),
-    { [getFullClassName('active')]: index === context.currentActiveIndex },
+    { [getFullClassName('active')]: index === context.currentActiveIndex, 'menu-opened': menuOpen },
     className,
   );
 
@@ -51,7 +52,7 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
       : {};
 
   const renderChildren = () => {
-    const subMenuClasses = classnames(getFullClassName('list'), { 'menu-opened': menuOpen });
+    const subMenuClasses = classnames(getFullClassName('list'));
     const childrenComponent = React.Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>;
       if (childElement.type.displayName === 'MenuItem')
@@ -64,6 +65,7 @@ const SubMenu: React.FC<SubMenuProps> = (props) => {
     <li className={classes} {...hoverEvents}>
       <div className={getFullClassName('title')} onClick={handleClick}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
